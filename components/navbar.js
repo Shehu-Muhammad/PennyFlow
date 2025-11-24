eacac1b9f004e982cc50b54275e965ae73c8fb28
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-11-24 08:43:58 -0500

    modified navbar component

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,6 @@
 export function createNavbar() {
   const nav = document.createElement('nav');
   nav.innerHTML = `
-    <a href="#" data-link="dashboard">Dashboard</a>
     <a href="#" data-link="income">Income</a>
     <a href="#" data-link="budget">Budget</a>
     <a href="#" data-link="safe-to-spend">Safe-to-Spend</a>
